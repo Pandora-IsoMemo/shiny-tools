84018376d296abd5ce9364ea2b7dfc9c3af1bdc2
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-03-22 20:23:33 +0100

    try fix test

--- tests/testthat/test-plotTitlesServer.R
+++ tests/testthat/test-plotTitlesServer.R
@@ -15,17 +15,19 @@ test_that("Test module plotTitlesServer", {
                  hide = FALSE
                )
 
-               expect_equal(session$returned %>% reactiveValuesToList(),
-                            list(plotTitle = list(text = "", fontType = "plain", color = "#000000",
-                                                  size = 12L, hide = FALSE),
-                                 xAxisTitle = list(text = "test", fontType = "bold",
-                                                   color = "#FFFFFF", size = 5, hide = FALSE),
-                                 yAxisTitle = list(text = "", fontType = "plain", color = "#000000",
-                                                   size = 12L, hide = FALSE),
-                                 xAxisText = list(fontType = "plain", color = "#000000", size = 10L,
-                                                  hide = FALSE),
-                                 yAxisText = list(fontType = "plain", color = "#000000", size = 10L,
-                                                  hide = FALSE))
+               expect_equal(session$returned[["xAxisTitle"]],
+                            list(text = "test", fontType = "bold", color = "#FFFFFF", size = 5,
+                                 hide = FALSE)
+                            # list(plotTitle = list(text = "", fontType = "plain", color = "#000000",
+                            #                       size = 12L, hide = FALSE),
+                            #      xAxisTitle = list(text = "test", fontType = "bold",
+                            #                        color = "#FFFFFF", size = 5, hide = FALSE),
+                            #      yAxisTitle = list(text = "", fontType = "plain", color = "#000000",
+                            #                        size = 12L, hide = FALSE),
+                            #      xAxisText = list(fontType = "plain", color = "#000000", size = 10L,
+                            #                       hide = FALSE),
+                            #      yAxisText = list(fontType = "plain", color = "#000000", size = 10L,
+                            #                       hide = FALSE))
                             )
 
                session$setInputs(
@@ -36,17 +38,18 @@ test_that("Test module plotTitlesServer", {
                  hide = FALSE
                )
 
-               expect_equal(session$returned %>% reactiveValuesToList(),
-                            list(plotTitle = list(text = "", fontType = "plain", color = "#000000",
-                                                  size = 12L, hide = FALSE),
-                                 xAxisTitle = list(text = "test", fontType = "bold",
-                                                   color = "#FFFFFF", size = 5, hide = FALSE),
-                                 yAxisTitle = list(text = "", fontType = "plain", color = "#000000",
-                                                   size = 12L, hide = FALSE),
-                                 xAxisText = list(fontType = "plain", color = "#000000", size = 10L,
-                                                  hide = FALSE),
-                                 yAxisText = list(fontType = "bold", color = "#FFFFFF", size = 5,
-                                                  hide = FALSE))
+               expect_equal(session$returned[["yAxisText"]],
+                            list(fontType = "bold", color = "#FFFFFF", size = 5, hide = FALSE)
+                            # list(plotTitle = list(text = "", fontType = "plain", color = "#000000",
+                            #                       size = 12L, hide = FALSE),
+                            #      xAxisTitle = list(text = "test", fontType = "bold",
+                            #                        color = "#FFFFFF", size = 5, hide = FALSE),
+                            #      yAxisTitle = list(text = "", fontType = "plain", color = "#000000",
+                            #                        size = 12L, hide = FALSE),
+                            #      xAxisText = list(fontType = "plain", color = "#000000", size = 10L,
+                            #                       hide = FALSE),
+                            #      yAxisText = list(fontType = "bold", color = "#FFFFFF", size = 5,
+                            #                       hide = FALSE))
                )
              })
 })

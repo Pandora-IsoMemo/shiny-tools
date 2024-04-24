55d07fd23f95fabb2b21e7d67b5e31ac7beac29b
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-04-24 17:23:15 +0200

    Feat/export list of data frames (#17)
    * extract plotExport from PlotR and resources
    
    * add test for plotExport
    
    * add UI to format titles of a ggplot before export
    
    * shift logic for titles into titlesServer, update test
    
    * add update of formating when switching the title
    
    * hide extraFormatting if not ggplot
    
    * update news.md, add config
    
    * rename functions
    
    * fix test
    
    * Feat/6 axis range (#9)
    
    * option to set the range of a ggplot before export
    
    * add test
    
    * fix checks
    
    * Feat/6 extract format g gplot (#10)
    
    * extract and export formatGGplot functions
    
    * rename functions
    
    * restructure parameters
    
    * rename function
    
    * fix name of function
    
    * fix name of config
    
    * add parameters titles and ranges to plotExport module
    
    * do not update titles or ranges if not set
    
    * fix max label size
    
    * add option to use ranges based on data
    
    * update labels
    
    * update params
    
    * fix issue that initial values were overwritten with default values
    
    * fix initRanges
    
    * fix test
    
    * extract module plotPoints from PlotR, add function formatPointsOfGGPlot()
    
    * add opacity, update config
    
    * add option to change the axis text to module and function
    
    * extract observeAndUpdateOf*Elements, add test module for plotRanges, fix docu
    
    * optional title for modules
    
    * update version
    
    * extract observeAndUpdatePointElements()
    
    * add new option "ggplot_only_titles" to plotExport
    
    * disable buttons for empty export
    
    * optionally hide inputs of plotPoints module
    
    * Feat/variable text elements (#16)
    
    * option to choose text elements
    
    * add message to formatTitlesOfGGplot()
    
    * add formatting for legend to formatTitlesOfGGPlot
    
    * remove getElementText from export
    
    * update news.md
    
    * option to use lists of dataframes in dataExport module
    
    * update news.md
    
    * export function exportXLSX
    
    * update description
    
    * fix check for export type
    
    * hide some tests

--- tests/testthat/test-dataExportServer.R
+++ tests/testthat/test-dataExportServer.R
@@ -16,3 +16,51 @@ test_that("Test module dataExportServer", {
                               am = "numeric", gear = "numeric", carb = "numeric"))
              })
 })
+
+test_that("Test exportXLSX", {
+  cars <- c("car1", "car2", "car3")
+  names(cars) <- cars
+  dataList <- lapply(cars, function(x) mtcars )
+
+  # test not available yet
+  # expect_no_error(exportXLSX(file = file.path(tempdir(), "test_file.xlsx"),
+  #                            dat = dataList[[1]]))
+  # expect_no_error(exportXLSX(file = file.path(tempdir(), "test_file.xlsx"),
+  #                            dat = dataList))
+  expect_error(exportXLSX(file = file.path(tempdir(), "test_file.xlsx"),
+                          dat = list()))
+})
+
+test_that("Test exportJSON", {
+  cars <- c("car1", "car2", "car3")
+  names(cars) <- cars
+  dataList <- lapply(cars, function(x) mtcars )
+
+  # test not available yet
+  # expect_no_error(exportJSON(file = file.path(tempdir(), "test_file.json"),
+  #                            dat = dataList[[1]]))
+  # expect_no_error(exportJSON(file = file.path(tempdir(), "test_file.json"),
+  #                            dat = dataList))
+  expect_error(exportJSON(file = file.path(tempdir(), "test_file.json"),
+                          dat = list(a = list())))
+})
+
+test_that("Test exportCSV", {
+  cars <- c("car1", "car2", "car3")
+  names(cars) <- cars
+  dataList <- lapply(cars, function(x) mtcars )
+
+  # test not available yet
+  # expect_no_error(exportCSV(file = file.path(tempdir(), "test_file.csv"),
+  #                           dat = dataList[[1]],
+  #                           colseparator = ", ",
+  #                           decseparator = "."))
+  # expect_no_error(exportCSV(file = file.path(tempdir(), "test_file.csv"),
+  #                           dat = dataList,
+  #                           colseparator = ", ",
+  #                           decseparator = "."))
+  expect_error(exportCSV(file = file.path(tempdir(), "test_file.csv"),
+                         dat = list(a = list()),
+                         colseparator = ", ",
+                         decseparator = "."))
+})

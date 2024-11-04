a71a516a7d7950213f99a8a1da60870d3508d9ce
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-11-04 08:30:29 +0100

    update namespace

--- R/00-NAMESPACE.R
+++ R/00-NAMESPACE.R
@@ -4,7 +4,7 @@
 #' @importFrom futile.logger flog.debug flog.info flog.warn
 #' @importFrom ggplot2 element_blank element_text geom_point ggplot_build ggtitle labs
 #'  scale_x_continuous scale_y_continuous sec_axis theme waiver xlab ylab
-#' @importFrom grDevices dev.off pdf png svg tiff
+#' @importFrom grDevices dev.off pdf png recordPlot svg tiff
 #' @importFrom lifecycle deprecate_warn deprecated is_present
 #' @importFrom magrittr %>%
 #' @importFrom openxlsx addWorksheet createWorkbook saveWorkbook writeData write.xlsx
@@ -15,7 +15,7 @@
 #' @importFrom shinyjs alert disable enable hide
 #' @importFrom shinyWidgets pickerInput updatePickerInput
 #' @importFrom stats lm
-#' @importFrom utils write.table
+#' @importFrom utils capture.output write.table
 #' @importFrom yaml yaml.load_file
 
 NULL

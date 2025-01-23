1ce766eabc8da08c94ce30649dda4db18c465bd7
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-01-23 10:00:27 +0100

    remove internal docu

--- R/01-onLoad.R
+++ R/01-onLoad.R
@@ -1,8 +1,8 @@
-#' Add path to images
-#'
-#' @param ... further arguments
-#'
-#' @return NULL
+# Add path to images
+#
+# @param ... further arguments
+#
+# @return NULL
 .onLoad <- function(...) {
   addResourcePath(prefix = "shinyTools_files",
                   directoryPath = system.file("dist", package = "shinyTools"))

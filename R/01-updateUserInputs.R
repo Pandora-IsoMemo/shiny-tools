e1affdd1172fa59c21d2695fa99c51e5e1a3257c
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-03-05 12:10:31 +0100

    add check and clean up to updateUserInputs()

--- R/01-updateUserInputs.R
+++ R/01-updateUserInputs.R
@@ -1,6 +1,7 @@
 #' Update User Inputs
 #'
-#' Update values from 'input' with all values from userInputs
+#' Update values from 'input' with all values from userInputs. Entries that are NULL are removed.
+#' Only updates entries that are present in 'input'.
 #'
 #' @param input input object from server function
 #' @param output output object from server function
@@ -9,6 +10,15 @@
 #'
 #' @export
 updateUserInputs <- function(input, output, session, userInputs) {
+  # check if userInputs is a list
+  if (!is.list(userInputs)) {
+    warning("Update of user inputs failed. 'userInputs' must be a list!")
+    return()
+  }
+
+  # remove NULL values, they cause upload of inputs to fail without warnings
+  userInputs <- userInputs[!sapply(userInputs, is.null)]
+
   ## get and filter input names
   inputIDs <- names(userInputs)
   inputIDs <- inputIDs[inputIDs %in% names(input)]
